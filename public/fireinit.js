eed78994117333f4a6774abc8bc8d4e2c0f98eef
Ryu <rjk5832@cambus.com>
2019-02-20 15:48:09 +0900

    test

---
+++ public/fireinit.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+// TODO: Replace with your project's customized code snippet
+var config = {
+  apiKey: "apiKey",
+  authDomain: "projectId.firebaseapp.com",
+  databaseURL: "https://databaseName.firebaseio.com",
+  storageBucket: "bucket.appspot.com",
+};
+firebase.initializeApp(config);
